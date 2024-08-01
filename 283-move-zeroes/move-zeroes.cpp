d68742bdc9386c4a6c87b82fda5258b062e8b693
Chikitsa <117767314+Chikitsa@users.noreply.github.com>
2024-08-01 12:09:55 +0530

    Time: 16 ms (54.00%) | Memory: 22.8 MB (5.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,23 +1,24 @@
-
-// Brute Force Solution
-
+//Optimal Solution
 class Solution {
 public:
-    void moveZeroes(std::vector<int>& nums) {
+    void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int lastNonZeroFoundAt = 0;
+        vector<int>temp;
+        // int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                temp.push_back(nums[i]);
 
-        
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] != 0) {
-                nums[lastNonZeroFoundAt] = nums[i];
-                lastNonZeroFoundAt++;
-            }
+            }    
+        }
+        for(int i=0;i<temp.size();i++){
+            nums[i]=temp[i];
         }
 
-        
-        for (int i = lastNonZeroFoundAt; i < n; ++i) {
-            nums[i] = 0;
+        int noofzeros = temp.size();
+        for(int i=noofzeros;i<n;i++){
+            nums[i]=0;
         }
+
     }
 };
