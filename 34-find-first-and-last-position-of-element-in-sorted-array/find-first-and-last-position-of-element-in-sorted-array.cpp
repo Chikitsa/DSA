c80b6c187040989df86203b84ab3f0ba53fb8d9e
Chikitsa <117767314+Chikitsa@users.noreply.github.com>
2024-08-06 00:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (34.49%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,49 @@
 class Solution {
 public:
-    int lowerBound(vector<int> arr, int n, int x) {
-    
-            int low=0,high=n-1;
-            int ans=n;
-            while(low<=high){
-                int mid=(low+high)/2;
-                if(arr[mid]>=x){
-                    ans=mid;
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            return ans;
-    }
-    int upperBound(vector<int> &arr, int n, int x){
-           
-        int low=0,high=n-1;
-        int ans=n;
+   
+    int firstOccurence(vector<int>& arr,int n,int k){
+        int low=0,high=n-1,first=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(arr[mid]>x){
-                ans=mid;
-                 high=mid-1;
-                
+            if(arr[mid] == k){
+                first=mid;
+                high=mid-1;
+            }
+            else if(arr[mid]<k){
+                low=mid+1;
+
             }
             else{
-                low=mid+1;
+                high=mid-1;
             }
-            }
-        return ans;
+            
+        }
+        return first;
+    }
+    int lastOccurence(vector<int>& arr,int n,int k){
+    int low=0,high=n-1,last=-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==k){
+            last=mid;
+            low=mid+1;
+        }
+        else if(arr[mid]<k){
+            low=mid+1;
+
+        }
+        else{
+            high=mid-1;
+        }      
+    }
+    return last;
     }
 
     vector<int> searchRange(vector<int>& arr, int k) {
         int n=arr.size();
-        int lb=lowerBound(arr,n,k);
-        if(lb==n ||arr[lb] != k) return{-1,-1};
-        return {lb,upperBound(arr,n,k)-1};
-
-        
-
-
-
-
-        
-   
-
-
+        int first=firstOccurence(arr,n,k);
+        if(first==-1) return {-1,-1};
+        int last=lastOccurence(arr,n,k);
+        return {first,last};
     }
 };
